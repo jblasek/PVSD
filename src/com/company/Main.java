be7b23eaadf5bd13605b9a52d8d6098474625606
jb838 <jacob.blasek1220@gmail.com>
2018-10-05 11:18:50 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Student stu1 = new Student("Ryan", 2.3);
+        Student stu2 = new Student("Brian", 3.4);
+        Student stu3 = new Student("Lion", 4.5 );
+
+        double[] gpaArray = new double[];
+        System.out.println();
+    }
+}
---
+++ src/com/company/Student.java
@@ -0,0 +1,19 @@
+package com.company;
+
+/**
+ * Created by jb838 on 10/5/18.
+ */
+public class Student {
+
+    String name;
+    double GPA;
+
+    public Student(String n, double g) {
+    name = n;
+    GPA = g;
+
+        }
+
+
+
+}
