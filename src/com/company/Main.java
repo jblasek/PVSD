78f29247d625e3a15a14a74c774cf9a32ea8070b
jblasek <jacob.blasek1220@gmail.com>
2018-10-08 22:24:28 -0400

    This is super inefficient but it kinda works

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,11 +4,106 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Student stu1 = new Student("Ryan", 2.3);
-        Student stu2 = new Student("Brian", 3.4);
-        Student stu3 = new Student("Lion", 4.5 );
+        Student stu1 = new Student("Kayla", 2.9);
+        Student stu2 = new Student("Jacob", 3.6);
+        Student stu3 = new Student("Alex", 4.0);
 
-        double[] gpaArray = new double[];
-        System.out.println();
+        if (stu1.GPA > stu2.GPA){
+
+            if (stu2.GPA > stu3.GPA ) {
+                System.out.println("The top three students are:");
+                System.out.println(stu1.name);
+                System.out.println(stu2.name);
+                System.out.println(stu3.name);
+            }
+
+            else if (stu2.GPA == stu3.GPA) {
+                System.out.println("The top three students are:");
+                System.out.println(stu1.name);
+                System.out.println(stu2.name + " and " + stu3.name + " have the same GPA!");
+            }
+
+            else if (stu2.GPA < stu3.GPA){
+
+                if (stu1.GPA > stu3.GPA) {
+                    System.out.println("The top three students are:");
+                    System.out.println(stu1.name);
+                    System.out.println(stu3.name);
+                    System.out.println(stu2.name);
+                }
+
+                else if (stu1.GPA == stu3.GPA) {
+                    System.out.println("The top three students are:");
+                    System.out.println(stu1.name + " and " + stu3.name + " have the same GPA!");
+                    System.out.println(stu2.name);
+                }
+
+                else {
+                    System.out.println("The top three students are:");
+                    System.out.println(stu3.name);
+                    System.out.println(stu1.name);
+                    System.out.println(stu2.name);
+                }
+            }
+        }
+
+        else if (stu1.GPA < stu2.GPA) {
+
+            if (stu1.GPA > stu3.GPA) {
+                System.out.println("The top three students are:");
+                System.out.println(stu2.name);
+                System.out.println(stu1.name);
+                System.out.println(stu3.name);
+            }
+
+            else if (stu1.GPA == stu3.GPA) {
+                System.out.println("The top three students are:");
+                System.out.println(stu2.name);
+                System.out.println(stu1.name + " and " + stu3.name + " have the same GPA!");
+            }
+
+            else if (stu3.GPA > stu1.GPA) {
+
+                if (stu3.GPA > stu2.GPA) {
+                    System.out.println("The top three students are:");
+                    System.out.println(stu3.name);
+                    System.out.println(stu2.name);
+                    System.out.println(stu1.name);
+                }
+
+                else if (stu3.GPA == stu2.GPA) {
+                    System.out.println("The top three students are:");
+                    System.out.println(stu2.name+ " and " + stu3.name + " have the same GPA!");
+                    System.out.println(stu1.name);
+                }
+
+                else {
+                    System.out.println("The top three students are:");
+                    System.out.println(stu2.name);
+                    System.out.println(stu3.name);
+                    System.out.println(stu1.name);
+            }
+
+
+            }
+        }
+        else {
+
+            if (stu1.GPA > stu3.GPA) {
+                System.out.println("The top three students are:");
+                System.out.println(stu1.name + " and " + stu2.name + " have the same GPA!");
+                System.out.println(stu3.name);
+            }
+
+            else if (stu1.GPA == stu3.GPA) {
+                System.out.println("There is a three way tie!! " + stu1.name + ", " + stu2.name + ", and " + stu3.name + " " + "have the same GPA!!!!");
+            }
+
+            else {
+                System.out.println("The top three students are:");
+                System.out.println(stu3.name);
+                System.out.println(stu1.name + " and " + stu2.name + " have the same GPA!");
+            }
+        }
     }
 }
